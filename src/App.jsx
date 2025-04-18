81de34670778404a22a65e0a9909004bb014b481
leontoys <svliyon@gmail.com>
2025-04-18 13:55:22 +1000

    polished ui

--- src/App.jsx
+++ src/App.jsx
@@ -4,108 +4,60 @@ import { HfInference } from "@huggingface/inference";
 
 function App() {
 
-  const [filePath,setFilePath] = useState("")
-  const [text,setText] = useState("")
+  const [filePath, setFilePath] = useState("")
+  const [text, setText] = useState("")
 
-  const handleFileChange = (e)=>{
-    console.log("filepath",e.target.files[0])
+  const handleFileChange = (e) => {
+    console.log("filepath", e.target.files[0])
     setFilePath(e.target.files[0])
   }
 
-  const fileLoaded = async (e)=>{
+  const fileLoaded = async (e) => {
 
     const result = e.target.result
 
     const hf_key = import.meta.env.VITE_HF
 
-    const client = new HfInference(hf_key); 
-  
+    const client = new HfInference(hf_key);
+
     //convert array buffer to blog
-    const data = new Blob([result],{ type: "audio/wav" })
-    
+    const data = new Blob([result], { type: "audio/wav" })
+
     try {
 
       const output = await client.automaticSpeechRecognition({
         data,
         model: "openai/whisper-large-v3",
         provider: "hf-inference",
-      });     
+      });
 
-      if(output){
+      if (output) {
         setText(output.text)
       }
-      else{
+      else {
         setText("Not able to transcribe at the moment.Please try later")
       }
-      
+
     } catch (error) {
 
-      console.error("Error",error)
-      
+      console.error("Error", error)
+      setText(error.message)
+
     }
-    
+
 
   }
 
-  const fileFetch = async(e)=>{
+  const handleFileUpload = async (e) => {
 
-    const result = filePath
-    console.log("result",result)
-
-    const hf_key = import.meta.env.VITE_HF
-  
-    //convert array buffer to blog
-    let data = new Blob([result],{ type: result.type })
-    console.log("audio-data",data)
-
-    data = data.slice(0,2000000)
-
-    const endpoint =
-    import.meta.env.MODE === 'development'
-      ? "/api/hf-inference/models/openai/whisper-large-v3-turbo"
-      : "https://router.huggingface.co/hf-inference/models/openai/whisper-large-v3-turbo"    
-      
-    console.log("endpoint",endpoint)
-    
-    try {
-      
-      const response = await fetch(
-        endpoint,
-        {
-          headers: {
-            Authorization: `Bearer ${hf_key}`,
-            "Content-Type": "application/json",
-          },
-          method: "POST",
-          body: data,
-        }
-      );
-
-      console.log("response",response)
-      
-      const transcript = await response.json();  
-
-      console.log("transcript",transcript)
-
-      setText(transcript.text)
-
-
-    } catch (error) {
-      console.error(error)
-    }
-
-  }
-
-  const handleFileUpload = async (e)=>{
-    
     const reader = new FileReader()
 
-    reader.addEventListener("load",fileFetch)
+    reader.addEventListener("load", fileLoaded)
 
     reader.readAsArrayBuffer(filePath)
-    
+
   }
-  
+
 
   return (
     <div className='App'>
